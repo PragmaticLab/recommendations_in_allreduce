297bfb38f0f92b7bc7972e7b63f4de3084b1dd91
Jason Xie <jxieeducation@gmail.com>
2016-06-27 21:10:02 -0700

    added nestrov

--- python_raw/matrix_factorization.py
+++ python_raw/matrix_factorization.py
@@ -38,10 +38,29 @@ class MF:
 			print "iter: %d, error: %f" % (e, self.currentError())
 			curr_alpha = alpha - (alpha - 0.00000001) * e / epoch
 			for i, (entry, score) in enumerate(zip(self.X, self.y)):
-				error = self.sgd(entry, score, curr_alpha)
+				error = self.nesterov_sgd(entry, score, curr_alpha)
 
-	def sgd(self, entry, score, alpha, momentum=0.9):
+	def momentum_sgd(self, entry, score, alpha, momentum=0.9):
 		r, p_entry, q_entry = self.predict(entry[0], entry[1])
+		e = score - r
+		p_grad = 2 * e * q_entry
+		q_grad = 2 * e * p_entry
+		if self.last_p_grad is not None:
+			p_grad = momentum * self.last_p_grad + alpha * p_grad
+			q_grad = momentum * self.last_q_grad + alpha * q_grad
+		p_entry += alpha * p_grad
+		q_entry += alpha * q_grad
+		self.last_p_grad = p_grad
+		self.last_q_grad = q_grad
+		return e
+
+	def nesterov_sgd(self, entry, score, alpha, momentum=0.9):
+		p_entry = self.p[entry[0]]
+		q_entry = self.q[entry[1]]
+		if self.last_p_grad is not None:
+			r = (p_entry + momentum * self.last_p_grad).dot(q_entry + momentum * self.last_q_grad)
+		else:
+			r = p_entry.dot(q_entry)
 		e = score - r
 		p_grad = 2 * e * q_entry
 		q_grad = 2 * e * p_entry
